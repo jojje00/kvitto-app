6ea439d78e6558d06fa3a7050e1ef98d1a7ac496
jojje00 <bolmstad@gmail.com>
2026-06-08 20:01:13 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+print("Kvitto-appen startar!")
+
+belopp = 150
+moms = belopp * 0.25
+totalt = belopp + moms
+
+print(f"Belopp: {belopp} kr")
+print(f"Moms: {moms} kr")
+print(f"Totalt: {totalt} kr")
